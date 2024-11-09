34bee961baf120a1e017f46940d7fcf574ab2d51
Jaewoo ahn <ahnjaewoo3042@gmail.com>
2024-11-09 22:35:28 +0900

    feat : user.model.ts

--- lib/database/index.ts
+++ lib/database/index.ts
@@ -1,9 +1,22 @@
 import mongoose from "mongoose";
 
-const MONGOBD_URL = process.env.MONGODB_URL;
+const MONGOBD_URI = process.env.MONGODB_URI;
 
 let cached = (global as any).mongoose || { conn: null, promise: null };
 
 export const connectToDatebase = async () => {
   if (cached.conn) return cached.conn;
+
+  if (!MONGOBD_URI) throw new Error("MONGODB_URI is missing");
+
+  cached.promise =
+    cached.promise ||
+    mongoose.connect(MONGOBD_URI, {
+      dbName: "Evently",
+      bufferCommands: false,
+    });
+
+  cached.conn = await cached.promise;
+
+  return cached.conn;
 };
