a5847dc682423fd607cc10c0fd2e3fb2f425dc48
Jaewoo ahn <ahnjaewoo3042@gmail.com>
2024-11-09 22:42:46 +0900

    feat : user model

--- lib/database/models/user.model.ts
+++ lib/database/models/user.model.ts
@@ -1,3 +1,14 @@
-import { Schema } from "mongoose";
+import { Schema, models, model } from "mongoose";
 
-const userSchema = new Schema({});
+const userSchema = new Schema({
+  clerkId: { type: String, required: true, unique: true },
+  email: { type: String, required: true, unique: true },
+  username: { type: String, required: true, unique: true },
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+  photo: { type: String, required: true },
+});
+
+const User = models.User || model("user", userSchema);
+
+export default User;
