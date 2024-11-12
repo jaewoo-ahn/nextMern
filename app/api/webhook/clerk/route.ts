fac46d9c32ae7b93d9e0ecdefb6324ddc3583c7d
Jaewoo ahn <ahnjaewoo3042@gmail.com>
2024-11-12 22:13:53 +0900

    fixed

--- app/api/webhook/clerk/route.ts
+++ app/api/webhook/clerk/route.ts
@@ -1,8 +1,8 @@
 import { Webhook } from "svix";
 import { headers } from "next/headers";
-import { WebhookEvent, clerkClient } from "@clerk/nextjs/server";
+import { WebhookEvent } from "@clerk/nextjs/server";
 import { createUser, deleteUser, updateUser } from "@/lib/actions/user.actions";
-
+import { clerkClient } from "@clerk/nextjs";
 import { NextResponse } from "next/server";
 
 export async function POST(req: Request) {
@@ -15,9 +15,9 @@ export async function POST(req: Request) {
   }
 
   const headerPayload = headers();
-  const svix_id = (await headerPayload).get("svix-id");
-  const svix_timestamp = (await headerPayload).get("svix-timestamp");
-  const svix_signature = (await headerPayload).get("svix-signature");
+  const svix_id = headerPayload.get("svix-id");
+  const svix_timestamp = headerPayload.get("svix-timestamp");
+  const svix_signature = headerPayload.get("svix-signature");
 
   if (!svix_id || !svix_timestamp || !svix_signature) {
     return new Response("Error occured -- no svix headers", {
@@ -56,8 +56,8 @@ export async function POST(req: Request) {
       clerkId: id,
       email: email_addresses[0].email_address,
       username: username!,
-      firstName: first_name!,
-      lastName: last_name!,
+      firstName: first_name,
+      lastName: last_name,
       photo: image_url,
     };
 
@@ -78,8 +78,8 @@ export async function POST(req: Request) {
     const { id, image_url, first_name, last_name, username } = evt.data;
 
     const user = {
-      firstName: first_name!,
-      lastName: last_name!,
+      firstName: first_name,
+      lastName: last_name,
       username: username!,
       photo: image_url,
     };
--- lib/database/index.ts
+++ lib/database/index.ts
@@ -1,17 +1,17 @@
 import mongoose from "mongoose";
 
-const MONGOBD_URI = process.env.MONGODB_URI;
+const MONGODB_URI = process.env.MONGODB_URI;
 
 let cached = (global as any).mongoose || { conn: null, promise: null };
 
 export const connectToDatabase = async () => {
   if (cached.conn) return cached.conn;
 
-  if (!MONGOBD_URI) throw new Error("MONGODB_URI is missing");
+  if (!MONGODB_URI) throw new Error("MONGODB_URI is missing");
 
   cached.promise =
     cached.promise ||
-    mongoose.connect(MONGOBD_URI, {
+    mongoose.connect(MONGODB_URI, {
       dbName: "Evently",
       bufferCommands: false,
     });
