f59104f7b81faaa8aa13dfcc3031351c17204cb5
Jaewoo ahn <ahnjaewoo3042@gmail.com>
2024-11-14 20:30:09 +0900

    components : Eventform.tsx

--- app/(root)/events/create/page.tsx
+++ app/(root)/events/create/page.tsx
@@ -1,8 +1,13 @@
 const CreateEvent = () => {
   return (
-    <section>
-      <h3 className="wrapper h3-bold text-center">Create Event</h3>
-    </section>
+    <>
+      <section className="bg-primary-50 bg-dotted-pattern bg-cover bg-center py-5 md:py-10">
+        <h3 className="wrapper h3-bold text-center sm:text-left">
+          Create Event
+        </h3>
+      </section>
+      <div className="wrapper my-8"></div>
+    </>
   );
 };
 
