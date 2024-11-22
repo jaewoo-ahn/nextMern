4b539b2531957c1ffe8fa59012b9155fa9023c00
Jaewoo ahn <ahnjaewoo3042@gmail.com>
2024-11-22 22:16:51 +0900

    feat : form

--- components/shared/EventForm.tsx
+++ components/shared/EventForm.tsx
@@ -39,7 +39,29 @@ type EventFormProps = {
 };
 
 const EventForm = ({ userId, type }: EventFormProps) => {
-  return <div>EventForm</div>;
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <FormField
+          control={form.control}
+          name="username"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Username</FormLabel>
+              <FormControl>
+                <Input placeholder="shadcn" {...field} />
+              </FormControl>
+              <FormDescription>
+                This is your public display name.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
+  );
 };
 
 export default EventForm;
