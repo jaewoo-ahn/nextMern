b4e4e60a6ea859bfd9222de7072796a5e3786c67
Jaewoo ahn <ahnjaewoo3042@gmail.com>
2024-11-24 20:02:09 +0900

    ui : alert-dialog

--- components/shared/Dropdown.tsx
+++ components/shared/Dropdown.tsx
@@ -1,4 +1,12 @@
-import React from "react";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { ICategory } from "@/lib/database/models/category.model";
+import { useState } from "react";
 
 type DropdownProps = {
   value?: string;
@@ -6,7 +14,28 @@ type DropdownProps = {
 };
 
 const Dropdown = ({ onChangeHandler, value }: DropdownProps) => {
-  return <div>Dropdown</div>;
+  const [categories, setCategoris] = useState<ICategory[]>([]);
+  return (
+    <Select onValueChange={onChangeHandler} defaultValue={value}>
+      <SelectTrigger className="select-field">
+        <SelectValue placeholder="Category" />
+      </SelectTrigger>
+      <SelectContent>
+        {categories.length &&
+          categories.map((category) => {
+            return (
+              <SelectItem
+                key={category._id}
+                value={category._id}
+                className="select-item p-regular-14"
+              >
+                {category.name}
+              </SelectItem>
+            );
+          })}
+      </SelectContent>
+    </Select>
+  );
 };
 
 export default Dropdown;
