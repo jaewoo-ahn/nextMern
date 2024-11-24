ee3d5177261cb7f033486f4616d35b9f03f679bd
Jaewoo ahn <ahnjaewoo3042@gmail.com>
2024-11-24 18:05:31 +0900

    shared : dropdown

--- components/shared/EventForm.tsx
+++ components/shared/EventForm.tsx
@@ -15,18 +15,7 @@ import {
 import { Input } from "@/components/ui/input";
 import { eventFormSchema } from "@/lib/validator";
 import * as z from "zod";
-
-const form = useForm<z.infer<typeof eventFormSchema>>({
-  resolver: zodResolver(eventFormSchema),
-  defaultValues: {
-    username: "",
-  },
-});
-
-// 2. Define a submit handler.
-function onSubmit(values: z.infer<typeof eventFormSchema>) {
-  console.log(values);
-}
+import { eventDefaultValues } from "@/constants";
 
 type EventFormProps = {
   userId: string;
@@ -34,25 +23,62 @@ type EventFormProps = {
 };
 
 const EventForm = ({ userId, type }: EventFormProps) => {
+  const initialValues = eventDefaultValues;
+
+  const form = useForm<z.infer<typeof eventFormSchema>>({
+    resolver: zodResolver(eventFormSchema),
+    defaultValues: initialValues,
+  });
+
+  function onSubmit(values: z.infer<typeof eventFormSchema>) {
+    console.log(values);
+  }
+
   return (
     <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-        <FormField
-          control={form.control}
-          name="username"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Username</FormLabel>
-              <FormControl>
-                <Input placeholder="shadcn" {...field} />
-              </FormControl>
-              <FormDescription>
-                This is your public display name.
-              </FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="flex flex-col gap-5"
+      >
+        <div className="flex flex-col gap-5 md:flex-row">
+          <FormField
+            control={form.control}
+            name="title"
+            render={({ field }) => (
+              <FormItem className="w-full">
+                <FormLabel>Username</FormLabel>
+                <FormControl>
+                  <Input
+                    placeholder="Event title"
+                    {...field}
+                    className="input-field"
+                  />
+                </FormControl>
+
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="categoryId"
+            render={({ field }) => (
+              <FormItem className="w-full">
+                <FormLabel>Username</FormLabel>
+                <FormControl>
+                  <Input
+                    placeholder="Event title"
+                    {...field}
+                    className="input-field"
+                  />
+                </FormControl>
+
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </div>
+
         <Button type="submit">Submit</Button>
       </form>
     </Form>
